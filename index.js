fb4ab653b4fa059756ef84a1dcb731b4df1c07de
David Alliger <alligerdavidj@gmail.com>
2022-04-27 21:22:10 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,67 @@
+window.addEventListener('DOMContentLoaded', event => {
+    const canvas = document.getElementById('canvas');
+    console.log(canvas);
+    const context = canvas.getContext('2d');
+
+    canvas.width = 1024;
+    canvas.height = 576;
+
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    class Sprite {
+        constructor({ position, velocity }) {
+            this.position = position;
+            this.velocity = velocity;
+            this.height = 150;
+        }
+
+        draw() {
+            context.fillStyle = 'red';
+            context.fillRect(this.position.x, this.position.y, 50, this.height)
+        }
+
+        update() {
+            this.draw();
+            this.position.y += this.velocity.y
+
+            if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+                this.velocity.y = 0;
+            }
+        }
+    }
+
+    const player = new Sprite({
+        position: {
+            x: 0,
+            y: 0
+        },
+        velocity: {
+            x: 0,
+            y: 10
+        }
+    });
+
+    const enemy = new Sprite({
+        position: {
+            x: 400,
+            y: 100
+        },
+        velocity: {
+            x: 0,
+            y: 0
+        }
+    });
+
+    function animate() {
+        window.requestAnimationFrame(animate);
+        context.fillStyle = 'black';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        player.update();
+        enemy.update();
+    }
+
+    animate();
+
+    console.log(player)
+    console.log(enemy)
+});
